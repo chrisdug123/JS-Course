7bc7ff0dd16cc6ec4c91e673c3981c627236bacd
chrisdug123 <139854947+chrisdug123@users.noreply.github.com>
2024-07-23 03:54:36 +0000

    Introduced while loops

---
+++ Loops/script.js
@@ -0,0 +1,21 @@
+// function sendSignal() {
+//   console.log('HELP!');
+// }
+
+// let i = 0;
+
+// /*we havent snet 1000 messages, continue*/
+// while (i < 1000) {
+//   sendSignal()
+//   i++;
+// }
+
+// let result = 0;
+
+// let j = 1;
+
+// while (j <= 10) {
+//   result = result + j;
+//   console.log(result)
+//   j++;
+// }

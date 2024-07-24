54d788e78f8611600abbace5fbdd698506e155f4
chrisdug123 <139854947+chrisdug123@users.noreply.github.com>
2024-07-24 01:34:00 +0000

    Intro to DOM API

---
+++ DOM API/script.js
@@ -0,0 +1,13 @@
+//1. Select
+let sign = document.getElementById('sign');
+console.log(sign)
+
+//2. Modify
+console.log(sign.textContent);
+sign.textContent = 'Welcome, travelers!';
+
+// modify
+// sign.innerHTML = sign.innerHTML + '<p>hello</p>';
+
+//3. Change style
+sign.style.color = 'blue';

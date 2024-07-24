da406489a47d1436e8f8b9e317a05e0198049b47
chrisdug123 <139854947+chrisdug123@users.noreply.github.com>
2024-07-24 01:09:47 +0000

    Iterating arrays

--- Arrays/script.js
+++ Arrays/script.js
@@ -1,19 +1,72 @@
-let database = ['turtle', 'cat', 'dog', 'bird'];
-console.log(database);
-console.log(database.length);
+// let database = ['turtle', 'cat', 'dog', 'bird'];
+// console.log(database);
+// console.log(database.length);
 
 
-let animal = database[0];
-console.log(animal);
+// let animal = database[0];
+// console.log(animal);
 
-database[0] = 'dinosaur';
-console.log(database);
+// database[0] = 'dinosaur';
+// console.log(database);
 
-let last = database[database.length - 1];
-console.log(last);
+// let last = database[database.length - 1];
+// console.log(last);
 
-database.push('lizard');
-console.log(database);
+// database.push('lizard');
+// console.log(database);
 
-database.pop()
-console.log(database);
+// database.pop()
+// console.log(database);
+
+
+let scores = [10, 20, 10];
+
+let i = 0;
+
+while (i < scores.length) {
+  scores[i]++;
+  i++;
+}
+
+console.log(scores)
+
+for (i = 0; i < scores.length; i++) {
+  scores[i]++;
+}
+
+console.log(scores);
+
+scores.forEach(function(entry, index) {
+  scores[index]++;
+})
+
+
+console.log(scores);
+
+
+let catalog = [
+  {
+    title: 'JS for Beginners',
+    author: 'Zenva',
+    copies: 1
+  },
+  {
+    title: 'HTML for Beginners',
+    author: 'XYX',
+    copies: 3
+  },
+  {
+    title: 'Python for Beginners',
+    author: 'Duggy',
+    copies: 1
+  }
+
+]
+
+catalog.forEach(function(entry, index) {
+  if (entry.author == 'Zenva') {
+    entry.copies++;
+  }
+});
+
+console.log(catalog);
